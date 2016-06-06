1f36c4ae373ee785cc46e267ac225e92b714f29d
mrcavalcanti <mccs-marcelo@uol.com.br>
2016-06-06 13:44:54 -0300

    test

--- demos/helloworld/index.php
+++ demos/helloworld/index.php
@@ -3,5 +3,6 @@
 // include Yii bootstrap file
 require_once(dirname(__FILE__).'/../../framework/yii.php');
 
+// test
 // create a Web application instance and run
 Yii::createWebApplication()->run();
