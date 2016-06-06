63f037f38ca90eeb6408844619466e9d95a6c1f0
mrcavalcanti <mccs-marcelo@uol.com.br>
2016-06-06 13:57:37 -0300

    teste 2
    teste 2 completo

--- demos/helloworld/index.php
+++ demos/helloworld/index.php
@@ -3,6 +3,6 @@
 // include Yii bootstrap file
 require_once(dirname(__FILE__).'/../../framework/yii.php');
 
-// test
+// test 2
 // create a Web application instance and run
 Yii::createWebApplication()->run();
